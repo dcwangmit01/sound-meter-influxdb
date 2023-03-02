2003273390ee72491afe3ce278b001db81b830ed
David C Wang <dcwangmit01@gmail.com>
2023-03-01 16:18:32 -0800

    Changed auto-formatter from yapf to black

--- nsrt_mk3_dev_collector.py
+++ nsrt_mk3_dev_collector.py
@@ -12,11 +12,11 @@ from shell import shell
 #####################################################################
 # Settings
 
-DEVICE_REGEX = 'usb-Convergence_Instruments_NSRT_mk3_Dev-*'
+DEVICE_REGEX = "usb-Convergence_Instruments_NSRT_mk3_Dev-*"
 WEIGHTING = nsrt_mk3_dev.NsrtMk3Dev.Weighting.DB_C
-TIMEZONE = 'America/Los_Angeles'
-LOGFILENAME = 'decible_measurements.csv'
-LOGDIR = 'measurements'
+TIMEZONE = "America/Los_Angeles"
+LOGFILENAME = "decible_measurements.csv"
+LOGDIR = "measurements"
 
 #######################################
 # Locate and open the sound meter device
@@ -31,8 +31,8 @@ nsrt = nsrt_mk3_dev.NsrtMk3Dev(DEVICE)
 model = nsrt.read_model()
 serial_number = nsrt.read_sn()
 firmware_revision = nsrt.read_fw_rev()
-date_of_birth = nsrt.read_dob().replace(' ', 'T')
-date_of_calibration = nsrt.read_doc().replace(' ', 'T')
+date_of_birth = nsrt.read_dob().replace(" ", "T")
+date_of_calibration = nsrt.read_doc().replace(" ", "T")
 
 #######################################
 # Ensure sound weighting configured on device
@@ -44,16 +44,19 @@ if weighting != WEIGHTING:
     weighting = nsrt.read_weighting()
     assert weighting == WEIGHTING, "unable to set weighting to " + WEIGHTING
 if weighting == WEIGHTING:  # Convert to printable string
-    weighting = 'dBC'
+    weighting = "dBC"
 
 #######################################
 # Gather Metadata and CSV columm header
 
 tags = [
-    "# measurement_name = decibels", f"# model = {model}",
-    f"# serial = {serial_number}", f"# firmware = {firmware_revision}",
+    "# measurement_name = decibels",
+    f"# model = {model}",
+    f"# serial = {serial_number}",
+    f"# firmware = {firmware_revision}",
     f"# date_manufacture = {date_of_birth}",
-    f"# date_calibration = {date_of_calibration}", f"# weighting = {weighting}"
+    f"# date_calibration = {date_of_calibration}",
+    f"# weighting = {weighting}",
 ]
 columns = ["timestamp", "leq_level", "weighted_level"]
 header = "\n".join(tags) + "\n" + ",".join(columns)
@@ -68,21 +71,31 @@ os.makedirs(LOGDIR, mode=0o775, exist_ok=True)
 # Custom Log File Rotator
 #   Creates a custom header composed of InfluxDB metadata and CSV column names
 class MyTimedRotatingFileHandler(TimedRotatingFileHandler):
-
-    def __init__(self,
-                 filename,
-                 when='h',
-                 interval=1,
-                 backupCount=0,
-                 encoding=None,
-                 delay=False,
-                 utc=False,
-                 atTime=None,
-                 errors=None,
-                 header=''):
+    def __init__(
+        self,
+        filename,
+        when="h",
+        interval=1,
+        backupCount=0,
+        encoding=None,
+        delay=False,
+        utc=False,
+        atTime=None,
+        errors=None,
+        header="",
+    ):
         self.header = header
-        super().__init__(filename, when, interval, backupCount, encoding,
-                         delay, utc, atTime, errors)
+        super().__init__(
+            filename,
+            when,
+            interval,
+            backupCount,
+            encoding,
+            delay,
+            utc,
+            atTime,
+            errors,
+        )
 
     def _open(self):
         stream = super()._open()
@@ -93,19 +106,19 @@ class MyTimedRotatingFileHandler(TimedRotatingFileHandler):
 
 
 # Silence an asyncio log message
-logging.getLogger('asyncio').setLevel(logging.WARNING)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
 
 # Configure logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(message)s',
-                    datefmt="%Y-%m-%dT%H:%M:%S%z")
+logging.basicConfig(
+    level=logging.DEBUG, format="%(message)s", datefmt="%Y-%m-%dT%H:%M:%S%z"
+)
 logger = logging.getLogger(__name__)
 
 # Create a logging handler
 #   Change to 'm' for minute when debugging
-handler = MyTimedRotatingFileHandler(LOGDIR + '/' + LOGFILENAME,
-                                     when='midnight',
-                                     header=header)
+handler = MyTimedRotatingFileHandler(
+    LOGDIR + "/" + LOGFILENAME, when="midnight", header=header
+)
 handler.suffix = "%Y%m%d"
 handler.extMatch = re.compile(r"^\d{8}$")
 logger.addHandler(handler)
@@ -115,7 +128,7 @@ logger.addHandler(handler)
 
 tz = pytz.timezone(TIMEZONE)
 
-while (True):
+while True:
     localized_date = tz.localize(datetime.now()).replace(microsecond=0)
     iso_date = localized_date.isoformat()
     leq_level = nsrt.read_leq()
